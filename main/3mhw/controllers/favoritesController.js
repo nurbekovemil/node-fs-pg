530507d07a0ec9cdc1093bb508acb495cc77500c
emil <nurbekovemil@mail.ru>
2021-04-05 21:55:28 +0600

    updated favorite route and controller files

--- main/3mhw/controllers/favoritesController.js
+++ main/3mhw/controllers/favoritesController.js
@@ -2,20 +2,22 @@ const client = require('../conf/db')
 
 const getFavoritesController = async (req, res) => {
   try {
-    const product_id = req.params.id, user_id = req.user.user_id
-    console.log(product_id, user_id)
-    res.status(200).send({product_id, user_id})
+    await client.query(`SELECT * FROM FAVORITES WHERE USER_ID = $1`,[req.user.user_id], (err, results) => {
+      if(err) return res.status(400).send({msg: 'Пусто! добавьте товар в избранное!'})
+      res.status(200).send({products: results.rows})
+    })
   } catch (error) {
-    res.status(400).send({msg:'Ошибка при добавлении!'})
+    res.status(400).send({msg:'Ошибка при получении данных!'})
   }
 }
 
 const addFavoritesController = async (req, res) => {
   try {
-    const {product_id} = req.body, user_id = req.user.user_id
+    const {product_id} = req.body, 
+          user_id = req.user.user_id
     await client.query(`
       INSERT INTO FAVORITES (USER_ID, PRODUCT_ID) VALUES ($1, $2)`, [user_id, product_id], (err) => {
-        if(err) return res.status(400).send({msg:'Ошибка при добавлении!'})
+        if(err) return res.status(400).send({msg:'Ошибка при добавлении данных!'})
         res.status(200).send({msg: 'Товар успешно добавлен!'})
       })
   } catch (error) {
--- main/3mhw/routes/favoritesRouter.js
+++ main/3mhw/routes/favoritesRouter.js
@@ -2,6 +2,6 @@ const authMiddleware = require('../middleware/authMiddleware')
 const {getFavoritesController, addFavoritesController} = require('../controllers/favoritesController')
 module.exports = favoritesRouter = async(fastify) => {
   fastify
-    .get('/:id',{preHandler: authMiddleware},getFavoritesController)
+    .get('/',{preHandler: authMiddleware},getFavoritesController)
     .post('/', {preHandler: authMiddleware}, addFavoritesController)
 }
